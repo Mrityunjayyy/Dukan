eb33458cad9e2e51801a4e33971d96b93560811a
Mrityunjay <mrityunjaysuman9@gmail.com>
2023-01-10 22:09:39 +0530

    I added the error to lear the revert git commit.

--- app/src/main/java/com/example/dukan/activities/AddEditAddressActivity.kt
+++ app/src/main/java/com/example/dukan/activities/AddEditAddressActivity.kt
@@ -2,11 +2,11 @@ package com.example.dukan.activities
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 import com.example.dukan.R
 import kotlinx.android.synthetic.main.activity_add_edit_address.*
-import kotlinx.android.synthetic.main.activity_address_list.*
 
-class AddEditAddressActivity : AppCompatActivity() {
+class AddEditAddressActivity : AppCompatActivity()  , View.OnClickListener{
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_add_edit_address)
@@ -20,6 +20,17 @@ class AddEditAddressActivity : AppCompatActivity() {
         actionBar?.setDisplayHomeAsUpEnabled(true)
 
         toolbar_add_edit_address_activity.setNavigationOnClickListener { onBackPressed() }
-
     }
+
+    override fun onClick(view : View?) {
+        if(view != null) {
+            when(view.id) {
+                R.id.btn_submit_address -> {
+
+                }
+            }
+        }
+    }
+
+  I am doing this to learn the revert git command
 }
